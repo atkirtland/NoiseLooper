306cf09027f62241f7a57c12c1f457961bd9e3be
Aaron Kirtland <atkirtland@gmail.com>
2026-07-07 13:51:25 -0400

    fixed syncing bars bug

--- app/src/main/java/ie/delilahsthings/soothingloop/MainActivity.java
+++ app/src/main/java/ie/delilahsthings/soothingloop/MainActivity.java
@@ -79,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
         SoundEffectVolumeManager.setOnPlayCallback(this::onPlaySounds);
         populateNoiselist();
         populateCustomNoiselist();
+        syncSeekBarsToManagerState();
 
         registerBroadcastReceivers();
 
